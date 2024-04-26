089821118f4ae475b7c6e882eb37610adb97d356
julia <julia4eck@gmail.com>
2024-04-26 08:25:12 +0300

    Edit Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,8 +3,11 @@ package org.example
 import org.yaml.snakeyaml.Yaml
 
 fun main() {
-    val yaml = Yaml()
-    val content = """
+    for (i in 0 until 10) {
+        val yaml = Yaml()
+        yaml.name = "MyYaml"
+        println(yaml.name)
+        val content = """
 A:
   B: 8
   C:
@@ -15,9 +18,17 @@ A:
         F2: abc
     G: 6
 """
-    val inputStream = content.byteInputStream()//File("src/resources/ex1.yaml").inputStream()
-    val data: Map<String, Any> = yaml.load(inputStream)
-    val strData = yaml.dump(data)
-    println("data.toString() = $data")
-    println("yaml.dump(data) = $strData")
+        val inputStream = content.byteInputStream()
+        for (j in 0 until i) {
+            val map = yaml.load<Map<String, Any>>(inputStream)
+            yaml.dump(map)
+            val str = yaml.load<String>(inputStream)
+            yaml.dump(str)
+        }
+        val data: Map<String, Any> = yaml.load(inputStream)
+        yaml.dump(data)
+        val strData = yaml.dump(data)
+        println("data.toString() = $data")
+        println("yaml.dump(data) = $strData")
+    }
 }
